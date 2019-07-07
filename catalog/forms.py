3369518562504bc2991d9c381f60eba21efa5b81
AlexValov <alexandrvalov@gmail.com>
2019-07-07 19:57:12 +0300

    add banner

--- catalog/forms.py
+++ catalog/forms.py
@@ -8,7 +8,7 @@ class UserForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = ('username', 'password', 'first_name', 'last_name', 'email')
+        fields = ('first_name', 'last_name', 'username', 'password', 'email' )
 
 
 class UserFormEdit(forms.ModelForm):
