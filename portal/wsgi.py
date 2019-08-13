61d9047e5250f55337aebbeb7cbe5237f0efe906
AlexValov <alexandrvalov@gmail.com>
2019-08-13 22:57:13 +0300

    whitenoise version change

--- portal/wsgi.py
+++ portal/wsgi.py
@@ -15,6 +15,3 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portal.settings')
 
 application = get_wsgi_application()
 
-#whitenoise on heroku
-from whitenoise.django import DjangoWhiteNoise
-application = DjangoWhiteNoise(application)
