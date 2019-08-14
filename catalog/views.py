e92ef83e6380a0a21b9a1884b82337d3bda7ac5d
AlexValov <alexandrvalov@gmail.com>
2019-08-14 22:14:02 +0300

    'test'

--- catalog/views.py
+++ catalog/views.py
@@ -37,8 +37,15 @@ def search_query(request):
 
 
 def category_in_menu(request):
+    generalcategories = GeneralCategory.objects.all()
+    categories = Category.objects.all()
+    subcategories = SubCategory.objects.all()
 
-    return render (request, 'catalog\category_in_menu.html')
+    return render (request, 'category_in_menu.html', {
+        'generalcategories':  generalcategories,
+        'categories': categories,
+        'subcategories': subcategories
+    })
 
 
 def sign_up(request):
